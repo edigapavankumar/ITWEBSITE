ccc0e0ed2165db30599fc343e06f413f2a5975d4
Pavan <pavan.ediga@meenews.co>
2025-02-07 05:27:42 +0530

    tailwind updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,70 +1,67 @@
 import React from "react";
 import Home from "./pages/Home";
 import Navbar1 from "./components/NavBar/Navbar";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import Login from "./pages/Login";
 import Login1 from "./pages/Login2step";
 import DashboardLayoutBasic from "./components/DashboardComponent/Employee";
 import Footer from "./components/Footer/Footer"; // Import Footer
 import Home1 from "./pages/Home1";
 import Login from "./pages/Login.jsx";
-import {EmployeeListPage} from "./pages/getEmployees.jsx";
-import {EmployeeContextProvider} from "./context/EmployeesContext";
-import { Routes, Route } from 'react-router-dom';
-import Sidebar from './components/sidebar/Sidebar';
+import { EmployeeListPage } from "./pages/getEmployees.jsx";
+import { EmployeeContextProvider } from "./context/EmployeesContext";
+import Sidebar from "./components/sidebar/Sidebar";
 // Admin Pages
-import Usermanagement from './components/DashboardComponent/admin/Usermanagement';
-import Contentmanagement from './components/DashboardComponent/admin/Contentmanagement';
-import Taskmanagement from './components/DashboardComponent/admin/Taskmanagement';
-import Notificationmanagement from './components/DashboardComponent/admin/Notificationmanagement';
-import Financialmanagement from './components/DashboardComponent/admin/Financialmanagement';
+import Usermanagement from "./components/DashboardComponent/admin/Usermanagement";
+import Contentmanagement from "./components/DashboardComponent/admin/Contentmanagement";
+import Taskmanagement from "./components/DashboardComponent/admin/Taskmanagement";
+import Notificationmanagement from "./components/DashboardComponent/admin/Notificationmanagement";
+import Financialmanagement from "./components/DashboardComponent/admin/Financialmanagement";
 
 // Employee Pages
-import Notificationmanagementemp from './components/DashboardComponent/emp/Notificationmanagementemp';
-import Taskmanagementemp from './components/DashboardComponent/emp/Taskmanagementemp';
-import Performancemanagementemp from './components/DashboardComponent/emp/Performancemanagementemp';
+import Notificationmanagementemp from "./components/DashboardComponent/emp/Notificationmanagementemp";
+import Taskmanagementemp from "./components/DashboardComponent/emp/Taskmanagementemp";
+import Performancemanagementemp from "./components/DashboardComponent/emp/Performancemanagementemp";
 
 // Third-party Pages
-import TaskOverviewThird from './components/DashboardComponent/thirdparty/TaskOverviewThird';
-import EarningsManagementThird from './components/DashboardComponent/thirdparty/EarningsManagementThird';
-import PerformanceMetricsThird from './components/DashboardComponent/thirdparty/PerformanceMetricsThird';
-import CommunicationThird from './components/DashboardComponent/thirdparty/CommunicationThird';
+import TaskOverviewThird from "./components/DashboardComponent/thirdparty/TaskOverviewThird";
+import EarningsManagementThird from "./components/DashboardComponent/thirdparty/EarningsManagementThird";
+import PerformanceMetricsThird from "./components/DashboardComponent/thirdparty/PerformanceMetricsThird";
+import CommunicationThird from "./components/DashboardComponent/thirdparty/CommunicationThird";
 
-// home
-import Home from './pages/Home';
 function App() {
-  const role = 'thirdparty';
-
+  const role = "thirdparty";
   const login = true;
   return (
-    <div className="app flex">
-      <Sidebar role={role} />
-      <div className="w-full p-6">
-        <Routes>
-          <Route path="/" element={<Home />} />
+    <div className="app">
+      {/* <Navbar1 /> */}
+      {/* <Sidebar role={role} /> */}
+      {/* <div className="w-full p-6"> */}
+        {/* <Routes> */}
+          {/* <Route path="/" element={<Home />} /> */}
           {/* Admin Routes */}
-          {role === 'admin' && (
-            <>
-              <Route path="/usermanagement" element={<Usermanagement />} />
-              <Route
-                path="/contentmanagement"
-                element={<Contentmanagement />}
-              />
-              <Route path="/taskmanagement" element={<Taskmanagement />} />
-              <Route
-                path="/notificationmanagement"
-                element={<Notificationmanagement />}
-              />
-              <Route
-                path="/financialmanagement"
-                element={<Financialmanagement />}
-              />
-            </>
-          )}
+          {/* {role === "admin" && ( */}
+            {/* <> */}
+              {/* <Route path="/usermanagement" element={<Usermanagement />} /> */}
+              {/* <Route */}
+                {/* path="/contentmanagement" */}
+                {/* element={<Contentmanagement />} */}
+              {/* /> */}
+              {/* <Route path="/taskmanagement" element={<Taskmanagement />} /> */}
+              {/* <Route */}
+                {/* path="/notificationmanagement" */}
+                {/* element={<Notificationmanagement />} */}
+              {/* /> */}
+              {/* <Route */}
+                {/* path="/financialmanagement" */}
+                {/* element={<Financialmanagement />} */}
+              {/* /> */}
+            {/* </> */}
+          {/* )} */}
 
           {/* Employee Routes */}
-          {role === 'emp' && (
-            <>
+          {/* {role === "emp" && ( */}
+            {/* <>
               <Route
                 path="/taskmanagementemp"
                 element={<Taskmanagementemp />}
@@ -77,11 +74,11 @@ function App() {
                 path="/notificationmanagementemp"
                 element={<Notificationmanagementemp />}
               />
-            </>
-          )}
+            </> */}
+          {/* )} */}
 
           {/* Third-Party Routes */}
-          {role === 'thirdparty' && (
+          {/* {role === "thirdparty" && (
             <>
               <Route
                 path="/taskoverviewthird"
@@ -102,27 +99,25 @@ function App() {
             </>
           )}
         </Routes>
-      </div>
+      </div> */}
       <Navbar1 />
       <EmployeeContextProvider>
-      <Routes>
-        <Route path="/test" element={<DashboardLayoutBasic />} />
-        <Route path="/home" element={<Home1 />} />
-        <Route path="/login"element={<Login/>} />
-        <Route path="/login1" element={<Login1 />} />
-        <Route path="/home1" element={<Home1 />} />
-       
-        <Route path="/getemp" element={<EmployeeListPage/>} />
-        
-      </Routes>
+        <Routes>
+          <Route path="/test" element={<DashboardLayoutBasic />} />
+          <Route path="/" element={<Home1 />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/login1" element={<Login1 />} />
+          {/* <Route path="/home1" element={<Home1 />} /> */}
+
+          <Route path="/getemp" element={<EmployeeListPage />} />
+        </Routes>
       </EmployeeContextProvider>
       {/* <Home1 /> */}
       <Footer />
     </div>
   );
-};
+}
 
 export default App;
-
 
 //https://chatgpt.com/share/67a50a63-bc1c-800d-b12f-7c2e22e2203b
