a327b02e0b000cc01f94b77f428129d8d7a7bf57
syedmohammedzubair1 <syedmohammedzubair913@gmail.com>
2025-02-07 12:50:08 +0530

    Added Tasks CRUD operations

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,14 +4,16 @@ import userRouter from "./routes/user.route.js";
 import "./database/dbconfig.js";
 import dotenv from "dotenv";
 import companyRouter from "./routes/comp.route.js";
+import taskRouter from "./routes/task.route.js";
 dotenv.config()
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use("v1/api",userRouter);
-app.use("v1/api",companyRouter);
+app.use("/api/v1",userRouter);
+app.use("/api/v1",companyRouter);
+app.use("/api/v1",taskRouter);
 app.listen(process.env.PORT,()=>{
     console.log("server is running on port 4000");
 });
