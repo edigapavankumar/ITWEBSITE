27aa3f694287c924c2b6f7782d56ffa5d46d12f8
Pavan <pavan.ediga@meenews.co>
2025-02-10 14:19:20 +0530

    deleted

--- backend/src/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import {Login} from "../controllers/auth.controller.js";
-
-const authRoutes = new Router();
-
-authRoutes.route("/login").post(Login)
-
-
-export default authRoutes
