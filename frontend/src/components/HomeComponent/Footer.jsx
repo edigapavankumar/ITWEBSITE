b434c60ec69a1601f60c6c2d3f4d172fd55b6d69
EDIGA PAVAN KUMAR <64640403+edigapavankumar@users.noreply.github.com>
2025-02-11 12:46:34 +0530

    Delete frontend directory

--- frontend/src/components/HomeComponent/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <div><footer className="footer">
-    <p>&copy; {new Date().getFullYear()} Our Company. All Rights Reserved.</p>
-  </footer></div>
-  )
-}
